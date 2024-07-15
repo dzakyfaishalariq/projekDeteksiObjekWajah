c393cc0016836dd56ae6503b0be15e27b6e2a3ce
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2024-07-15 22:03:52 +0700

    deteksi objek

--- src/model/yolov10Model.py
+++ src/model/yolov10Model.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 from ultralytics import YOLO
 
 def modelYolo(frame):
     load_model = YOLO("./deteksiMuka/src/model/best.pt")
     prediksi = load_model.predict(frame)
-=======
-from ultralytics import YOLO
-
-def modelYolo(frame):
-    load_model = YOLO("./deteksiMuka/src/model/best.pt")
-    prediksi = load_model.predict(frame)
->>>>>>> 94a3114 (deteksi objek wajah)
     return prediksi
